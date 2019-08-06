6eed3cf7273622c07dd9cd80f206ecd97431805c
李康qazwsx <goodMorning_glb@likang.com>
2019-08-06 15:49:06 +0800

    aaaa

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,8 +1,9 @@
-package PACKAGE_NAME;
-
 /**
  * @author likang
  * @create 2019-08-06-14:19
  */
 public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("aaaaaa");
+    }
 }
